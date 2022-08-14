c495e53b2de72abeb314665d8b135bdd31cba4c7
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-14 19:01:02 +0100

    TA prep: index front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Erasing ingredients..."
+puts "Erasing everything"
 
+Dose.destroy_all
 Ingredient.destroy_all
+Cocktail.destroy_all
 
 puts "Fetching ingredients..."
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-json_serialized = URI.open(url).read
-ingredient_json = JSON.parse(json_serialized)
+ingredients_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = URI.open(ingredients_url).read
+ingredient_json = JSON.parse(ingredients_serialized)
 
 ingredient_json["drinks"].each do |ingredient|
-  Ingredient.new(name: ingredient["strIngredient1"])
+  Ingredient.create!(name: ingredient["strIngredient1"])
   puts "Created #{ingredient["strIngredient1"]}"
 end
 
 puts "Ingredients created"
+
+puts "Creating cocktails"
+
+10.times do
+  random_cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  cocktail_serialized = URI.open(random_cocktail_url).read
+  cocktail_json = JSON.parse(cocktail_serialized)
+  cocktail = Cocktail.create!(name: cocktail_json["drinks"][0]["strDrink"])
+  puts "Created #{cocktail.name}"
+end
